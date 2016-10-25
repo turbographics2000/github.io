cedc3585363a552e4ff13b978ecacf03939fc3b7
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-10-25 18:14:34 +0900

    コミット

--- webrtc.js
+++ webrtc.js
@@ -115,7 +115,7 @@ function displayReport(report) {
     const h1 = document.createElement('h1');
     h1.textContent = '統計情報';
     container.appendChild(h1);
-    const tableRow = (tbody, cols, cellType) => {
+    const tableRow = (tbody, cols, cellType) => { 
         const tr = document.createElement('tr');
         cols.forEach(col => {
             const td = document.createElement(cellType);
