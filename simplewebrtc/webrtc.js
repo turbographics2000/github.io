d37c8c1173fad19c93f34cc837293d006bffbb74
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-10-26 17:45:47 +0900

    レポート取得と画面表示ロジックを混在化

--- simplewebrtc/webrtc.js
+++ simplewebrtc/webrtc.js
@@ -94,22 +94,90 @@ function logError(error) {
     console.log(error.name + ": " + error.message);
 };
 
+// function chromeGetStats() {
+//     pc.getStats(response => {
+//         const standardReport = {};
+//         response.result().forEach(stats => {
+//             const standardStats = {
+//                 id: stats.id,
+//                 timestamp: stats.timestamp,
+//                 type: stats.type
+//             };
+//             stats.names().forEach(name => {
+//                 standardStats[name] = stats.stat(name);
+//             });
+//             standardReport[stats.type] = standardReport[stats.type] || {};
+//             standardReport[stats.type][standardStats.id] = standardStats;
+//         });
+//         displayReport(standardReport);
+//     });
+// }
+
+const standardReport = {};
 function chromeGetStats() {
     pc.getStats(response => {
-        let standardReport = {};
-        response.result().forEach(reports => {
-            const standardStats = {
-                id: reports.id,
-                timestamp: reports.timestamp,
-                type: reports.type
-            };
-            reports.names().forEach(name => {
-                standardStats[name] = reports.stat(name);
+        response.result().forEach(stats => {
+            const typeId = 'webrtc_statstype' + stats.type;
+            let statsTypeContainer = window[typeId];
+            let reportStatsType = standardReport[typeId];
+            if(!reportStatsType) {
+                statsTypeContainer = document.createElement('div');
+                statsTypeContainer.id = typeId;
+                const h2 = document.createElement('h2');
+                h2.textContent = typ + ` タイプ (cnt: ${Object.keys(report[typ]).length})`;
+                statsTypeContainer.appendChild(h2);
+
+                reportStatsType = {};
+                standardReport[statsTypeId] = reportStatsType;
+            } 
+
+            const statsId = 'webrtc_stats' + sstats.Id;
+            let statsTable = window[statsId];
+            let reportStats = reportStatsType[statsId];
+            const statsTableId = 'stats' + stats.id;
+            if(!reportStats) {
+                const table = document.createElement('table');
+                const tHead = document.createElement('thead');
+                const thtr = document.createElement('tr');
+                const th = document.createElement('th');
+                const tBody = document.createElement('tbody');
+                th.colSpan = 2;
+                th.textContent = stats.id;
+                thtr.appendChild(th);
+                thead.appendChild(thtr);
+                table.appendChild(thead);
+                tBody.id = statsId;
+                table.appendChild(tbody);
+                statsTable = tBody;
+                statsTypeContainer.appendChild(table);
+
+                reportStats = {
+                    id: stats.id,
+                    timestamp: stats.timestamp,
+                    type: stats.type
+                };
+            }
+            
+            stats.names().forEach(name => {
+                const statId = 'webrtc_stat' + name;
+                const value = stats.stat(name);
+                let oldValue = reportStats[name];
+                if(!oldValue) {
+                    const tr = document.createElement('tr');
+                    const tdName = document.createElement('td');
+                    const tdValue = document.createElement('td');
+                    tdName.textContent = name;
+                    tdValue.id = statId;
+                    tdValue.textContent = stats.stat(name);
+                    tr.appendChild(tdName);
+                    tr.appendChild(tdValue);
+                    statsTable.appendChild(tr);
+                } else if(oldValue !== value) {
+                    window[statId].textContent = value;
+                }
+                reportStats[name] = value;
             });
-            standardReport[reports.type] = standardReport[reports.type] || {};
-            standardReport[reports.type][standardStats.id] = standardStats;
         });
-        displayReport(standardReport);
     });
 }
 
@@ -127,6 +195,7 @@ function displayReport(report) {
             tr.appendChild(td);
         });
         tbody.appendChild(tr);
+        return tr;
     }
     for(let typ in report) {
         const h2 = document.createElement('h2');
@@ -136,15 +205,30 @@ function displayReport(report) {
             //const h3 = document.createElement('h3');
             //h3.textContent = statsId;
             //container.appendChild(h3);
-            const table = document.createElement('table');
-            const tHead = document.createElement('thead');
-            const tBody = document.createElement('tbody');
-            table.appendChild(tHead);
-            table.appendChild(tBody);
-            tableRow(tHead, [statsId], 'th');
+            const tableId = 'table' + statsId;
+            let table = winodw[tableId];
+            if(!table) {
+                table = document.createElement('table');
+                table.id = tableId;
+                const tHead = document.createElement('thead');
+                const tBody = document.createElement('tbody');
+                table.appendChild(tHead);
+                table.appendChild(tBody);
+                tableRow(tHead, 'th', statsId, [statsId]);
+            }
             for(let name in report[typ][statsId]) {
-                if(name !== 'id' && name !== 'type')
-                    tableRow(tBody, [name, report[typ][statsId][name]], 'td');
+                if(name !== 'id' && name !== 'type') {
+                    const rowId = `row${statsId + name}`;
+                    const value = report[typ][statsId][name];
+                    let row = window[rowId];
+                    if(row) {
+                        if(row.dataset.value !== value) {
+
+                        }
+                    } else {
+                        tableRow(tBody, 'td', rowId, [name, value]);
+                    }
+                }
             }
             container.appendChild(table);
         }
